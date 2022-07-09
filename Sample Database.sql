f7ac39d22153ec70586489627947122699553458
udayshekar26 <98219656+udayshekar26@users.noreply.github.com>
2022-07-09 07:50:34 +0530

    Add files via upload

---
+++ Sample Database.sql
@@ -0,0 +1,14 @@
+show databases;
+use studentdb;
+show tables;
+select * from account;
+select * from branch;
+select * from course;
+select * from customer;
+select * from enrolled;
+select * from faculty;
+select * from stud;
+select * from student;
+select * from student1;
+
+
